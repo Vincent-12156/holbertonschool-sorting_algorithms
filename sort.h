4692b3ce1c772759487f5273410ae73a337f26b6
EnzoMessaoudi <12168@holbertonstudents.com>
2025-12-12 17:07:40 +0100

    Fix header file

--- sort.h
+++ sort.h
@@ -24,5 +24,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int pivot_index(int *array, int start, int end, size_t size);
+void quick_sort_lomuto(int *array, int start, int end, size_t size);
 
 #endif
