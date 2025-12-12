bf27d797d884884ad5dd8b1c197e602cd9bf4ef0
Vincent-12156 <12156@holbertonstudents.com>
2025-12-12 10:22:03 +0100

    Change header.h

--- sort.h
+++ sort.h
@@ -1,12 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
 
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-
-
 #include <stdlib.h>
 #include <stdio.h>
 
